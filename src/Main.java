e0748155b271976a2c1d4dc25b02de0651a99ecb
amba404 <amba.avg@gmail.com>
2024-09-05 09:56:45 +0900

    HW done. With fixed Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,102 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\nTask 1\n");
+        {
+            int total = 0;
+            int target = 2_459_000;
+            int monthDeposit = 15_000;
+            int month = 0;
+
+            while (total < target) {
+                month++;
+                total += total /100 + monthDeposit;
+                System.out.printf("Месяц %d, сумма накоплений равна %d рублей\n", month, total);
+            }
+        }
+
+        System.out.println("\nTask 2\n");
+        {
+            int i = 0;
+            while (i < 10) {
+                i++;
+                System.out.printf("%d ", i);
+            }
+            System.out.println();
+
+            for (; i > 0; i--) {
+                System.out.printf("%d ", i);
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nTask 3\n");
+        {
+            int populationY = 12_000_000;
+            int birthFor1K = 17;
+            int deathFor1K = 8;
+
+            for (int i = 1; i < 11; i++) {
+                populationY += populationY / 1000 * (birthFor1K - deathFor1K);
+                System.out.printf("Год %d, численность населения составляет %d\n", i, populationY);
+            }
+        }
+
+        System.out.println("\nTask 4\n");
+        {
+            int deposit = 15_000;
+            int target = 12_000_000;
+            int month = 0;
+
+            while (deposit < target) {
+                month++;
+                deposit *= 1.07;
+                System.out.printf("Месяц %d, накопленная сумма %d\n", month, deposit);
+            }
+        }
+
+        System.out.println("\nTask 5\n");
+        {
+            int deposit = 15_000;
+            int target = 12_000_000;
+            int month = 0;
+
+            while (deposit < target) {
+                month++;
+                deposit *= 1.07;
+                if (month % 6 == 0)
+                    System.out.printf("Месяц %d, накопленная сумма %d\n", month, deposit);
+            }
+        }
+
+        System.out.println("\nTask 6\n");
+        {
+            int deposit = 15_000;
+
+            for (int month = 1; month < 9 * 12 + 1; month++) {
+                deposit *= 1.07;
+                if (month % 6 == 0)
+                    System.out.printf("Месяц %d, накопленная сумма %d\n", month, deposit);
+            }
+        }
+
+        System.out.println("\nTask 7\n");
+        {
+            int firstFriday = 2;
+
+            for (int i = firstFriday; i < 31; i = i + 7)
+                System.out.printf("Сегодня пятница, %d-е число. Необходимо подготовить отчет\n", i);
+        }
+
+        System.out.println("\nTask 8\n");
+        {
+            int nowYear = 2024;
+
+            for (int i = nowYear - 200; i < nowYear + 100; i++) {
+                if (i % 79 == 0) {
+                    System.out.println(i);
+                    i += 78;
+                }
+            }
+        }
     }
 }
